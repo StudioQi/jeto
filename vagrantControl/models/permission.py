2c03ab3ca32a9f092ad6e2b791f5401c56fac1c4
Pierre Paul Lefebvre <info@pierre-paul.com>
2014-07-31 15:01:56 -0400

    Its better with all the files

---
+++ vagrantControl/models/permission.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+from collections import namedtuple
+from functools import partial
+from flask.ext.login import current_user
+from flask.ext.principal import identity_loaded, Permission, RoleNeed, UserNeed
+
+from vagrantControl import db
+
+InstanceNeed = namedtuple('instance', ['method', 'value'])
+StartInstanceNeed = partial(InstanceNeed, 'start')
+StopInstanceNeed = partial(InstanceNeed, 'stop')
+ProvisionInstanceNeed = partial(InstanceNeed, 'provision')
+DestroyInstanceNeed = partial(InstanceNeed, 'destroy')
+ViewInstanceNeed = partial(InstanceNeed, 'view')
+
+HostNeed = namedtuple('instance', ['method', 'value'])
+ViewHostNeed = partial(HostNeed, 'view')
+
+class StartInstancePermission(Permission):
+    def __init__(self, instanceId):
+        need = StartInstanceNeed(unicode(instanceId))
+        super(StartInstancePermission, self).__init__(need)
+
+class StopInstancePermission(Permission):
+    def __init__(self, instanceId):
+        need = StopInstanceNeed(unicode(instanceId))
+        super(StopInstancePermission, self).__init__(need)
+
+class ProvisionInstancePermission(Permission):
+    def __init__(self, instanceId):
+        need = ProvisionInstanceNeed(unicode(instanceId))
+        super(ProvisionInstancePermission, self).__init__(need)
+
+class DestroyInstancePermission(Permission):
+    def __init__(self, instanceId):
+        need = DestroyInstanceNeed(unicode(instanceId))
+        super(DestroyInstancePermission, self).__init__(need)
+
+class ViewInstancePermission(Permission):
+    def __init__(self, instanceId):
+        need = ViewInstanceNeed(unicode(instanceId))
+        super(ViewInstancePermission, self).__init__(need)
+
+class ViewHostPermission(Permission):
+    def __init__(self, instanceId):
+        need = ViewInstanceNeed(unicode(instanceId))
+        super(ViewInstancePermission, self).__init__(need)
+
+class ViewHostPermission(Permission):
+    def __init__(self, hostId):
+        need = ViewHostNeed(unicode(hostId))
+        super(ViewHostPermission, self).__init__(need)
+
+class TeamPermissionsGrids(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    objectId = db.Column(db.Integer)
+    objectType = db.Column(db.String(64))
+    action = db.Column(db.String(64))
+    team_id = db.Column(
+        db.Integer,
+        db.ForeignKey('team.id')
+    )
+
+class UserPermissionsGrids(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    objectId = db.Column(db.Integer)
+    objectType = db.Column(db.String(64))
+    action = db.Column(db.String(64))
+    user_id = db.Column(
+        db.String(64),
+        db.ForeignKey('user.id')
+    )
