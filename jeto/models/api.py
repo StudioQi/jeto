dcb74c003ec2a8d59e8045d7913bbc646afbeef2
Aurelien Bondis <abondis@pheromone.ca>
2015-09-15 17:22:32 -0400

    api keys model

--- jeto/models/api.py
+++ jeto/models/api.py
@@ -7,9 +7,9 @@ from jeto import db
 class APIKeys(db.Model):
     __tablename__ = 'api_keys'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    comment = db.Column(db.String)
+    name = db.Column(db.String(255))
+    comment = db.Column(db.String(255))
     user_id = db.Column(
-        db.Integer,
+        db.String(64),
         db.ForeignKey('user.id')
     )
