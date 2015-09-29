927c2dc8242ac290fcdb47a0a1e33948aa8f0d83
Pierre Paul Lefebvre <info@pierre-paul.com>
2015-09-29 16:01:28 -0400

    Super cool paging on the auditlogs

--- jeto/services/auditlog.py
+++ jeto/services/auditlog.py
@@ -1,13 +1,12 @@
 # -=- encoding: utf-8 -=-
 
-from flask.ext.restful import fields, marshal_with
-from jeto import db
+from flask import Response
+from flask.ext.restful import fields, marshal, request
+import json
+
+from jeto import db, app
 from jeto.models.auditlog import AuditLog
-from jeto.services import RestrictedResource  # , adminAuthenticate
-
-
-json_headers = {'Content-Type': 'application/json',
-                'Accept': 'application/json'}
+from jeto.services import RestrictedResource
 
 auditlog_fields = {
     'id': fields.Integer,
@@ -17,6 +16,31 @@ auditlog_fields = {
 
 
 class AuditlogApi(RestrictedResource):
-    @marshal_with(auditlog_fields)
     def get(self):
-        return db.session.query(AuditLog).all() or {}
+        PER_PAGE = 30
+        data = request.args
+
+        page = data.get('page', 1)
+        limit = int(data.get('limit', PER_PAGE))
+        offset = (int(page) - 1) * limit
+        order_by = data.get('order_y', 'start_date')
+        sorted = data.get('sorted', 'asc')
+        if sorted.upper() not in ['ASC', 'DESC']:
+            sorted = 'asc'
+
+        logs = AuditLog.\
+            query.\
+            order_by('{} {}'.format(order_by, sorted)).\
+            limit(limit).\
+            offset(offset).\
+            all()
+
+        data = [marshal(log, auditlog_fields) for log in logs]
+        app.logger.debug(json.dumps(data))
+        response = Response(json.dumps(data), status=200, mimetype='application/json')
+        response.headers['page'] = page
+        response.headers['limit'] = limit
+        response.headers['offset'] = offset
+        response.headers['order_by'] = order_by
+        response.headers['sorted'] = sorted
+        return response
